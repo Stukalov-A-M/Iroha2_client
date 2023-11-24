b10cda1b18e2986289619f05bd1b9209dbb860f1
Stukalov-A-M <stukalov07@gmail.com>
2023-11-24 16:30:37 +0100

    EDIT: Refactoring by comments

--- src/config.rs
+++ src/config.rs
@@ -1,9 +1,9 @@
+use iroha_config::client::Configuration;
 use std::fs::File;
 use std::path::Path;
-use iroha_config::client::Configuration;
 
 pub fn get_config() -> Configuration {
-        let path = Path::new("C:\\RustProjects\\iroha_stable\\src\\resources\\config.json");
-        let file = File::open(path).unwrap();
-        serde_json::from_reader(file).expect(&format!("Failed to read config at {:?}", &path))
+    let path = Path::new("C:\\RustProjects\\iroha_stable\\src\\resources\\config.json");
+    let file = File::open(path).unwrap();
+    serde_json::from_reader(file).unwrap_or_else(|_| panic!("Failed to read config at {path:?}"))
 }
--- src/examples/block_stream.rs
+++ src/examples/block_stream.rs
@@ -1,4 +1,3 @@
-mod config;
 use std::num::NonZeroU64;
 use iroha_client::client::Client;
 
@@ -11,31 +10,37 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 }
-// Auxiliary method for a block listener
-// You shall implement it first
-fn non_zero_handler(number: u64) -> NonZeroU64 {
-    NonZeroU64::new(number).map_or_else(|| {
-        println!("The number must be > 0, using default value 1");
-        NonZeroU64::new(1).unwrap()
-    }, |non_zero|non_zero)
-
+ fn get_config() -> Configuration {
+    let path = Path::new("C:\\RustProjects\\iroha_stable\\src\\resources\\config.json");
+    let file = File::open(path).unwrap();
+    serde_json::from_reader(file).unwrap_or_else(|_| panic!("Failed to read config at {path:?}"))
 }
-/*
-A block listener configuration
-    iroha_client(&Client) - Your iroha client implementation
-    initial_block_number(u65) - The number of a block listener should start from.
-    To get total quantity of blocks, you may use method iroha_client.get_status().
- */
-fn initiate_block_listener(iroha_client :&Client, initial_block_number: u64) {
+/// Auxiliary method for a block listener
+/// You shall implement it first
+fn non_zero_handler(number: u64) -> NonZeroU64 {
+    NonZeroU64::new(number).map_or_else(
+        || {
+            println!("The number must be > 0, using default value 1");
+            NonZeroU64::new(1).unwrap()
+        },
+        |non_zero| non_zero,
+    )
+}
+/// A block listener configuration
+/// iroha_client - Your iroha client implementation
+/// initial_block_number - The number of a block listener should start from.
+/// To get total quantity of blocks, you may use method iroha_client.get_status().
+fn initiate_block_listener(iroha_client: &Client, initial_block_number: u64) -> Result<(), Box<dyn std::error::Error>> {
     // Processing the non zero value from the u64
     let block_number = non_zero_handler(initial_block_number);
     // Initiating the block listener object
-    let block_listener = iroha_client.listen_for_blocks(block_number).unwrap();
+    let block_iter = iroha_client.listen_for_blocks(block_number)?;
     // Initiating iteration by blocks. The iterator is infinite
-    for block in block_listener {
+    for block in block_iter {
         match &block {
-            Ok(block) => println!("{}", block.payload().to_string()),
-            Err(e) => println!("Some error {}", e)
+            Ok(block) => println!("Received block: {}", block.payload().to_string()),
+            Err(e) => println!("Error happened: {}", e)
         }
     }
+    Ok(())
 }
