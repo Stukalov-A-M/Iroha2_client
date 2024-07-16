196687f0fd8babed5200d7622263b20190e475df
Stukalov-A-M <stukalov07@gmail.com>
2024-07-16 09:29:15 +0800

    Ready

--- src/smartcontracts/buy_bonds/src/lib.rs
+++ src/smartcontracts/buy_bonds/src/lib.rs
@@ -6,6 +6,8 @@ extern crate alloc;
 extern crate panic_halt;
 
 use alloc::borrow::ToOwned;
+use alloc::boxed::Box;
+use core::error::Error;
 
 use iroha_trigger::prelude::*;
 use lol_alloc::{FreeListAllocator, LockedAllocator};
@@ -13,11 +15,10 @@ use lol_alloc::{FreeListAllocator, LockedAllocator};
 #[global_allocator]
 static ALLOC: LockedAllocator<FreeListAllocator> = LockedAllocator::new(FreeListAllocator::new());
 
-use iroha_trigger::prelude::QueryBox::{FindAssetDefinitionById};
+use iroha_trigger::prelude::QueryBox::FindAssetDefinitionById;
 
 #[iroha_trigger::main]
 fn trigger_entry_point(_id: TriggerId, owner: AccountId, event: Event) {
-
     // Catch SetKeyValue event for the trigger
     let event = match event {
         Event::Data(DataEvent::Trigger(TriggerEvent::MetadataInserted(event))) => event,
@@ -26,7 +27,9 @@ fn trigger_entry_point(_id: TriggerId, owner: AccountId, event: Event) {
 
     // Check that event is related to bonds
     // and contains of "buy_bonds" container
-    if !(event.key().as_ref() == "buy_bonds") {return}
+    if event.key().as_ref() != "buy_bonds" {
+        return;
+    }
 
     // Defining the transaction properties
     let bond_asset_definition_id: AssetDefinitionId;
@@ -37,84 +40,126 @@ fn trigger_entry_point(_id: TriggerId, owner: AccountId, event: Event) {
 
     // Deconstruct the "buy_bonds" container
     if let Value::LimitedMetadata(metadata) = event.value() {
-        bond_asset_definition_id = metadata.get("bonds_id")
+        bond_asset_definition_id = metadata
+            .get("bonds_id")
             .dbg_unwrap()
             .to_owned()
             .try_into()
             .dbg_unwrap();
-        bonds_quantity = metadata.get("quantity")
+        bonds_quantity = metadata
+            .get("quantity")
             .dbg_unwrap()
             .to_owned()
             .try_into()
             .dbg_unwrap();
-        transaction_amount = metadata.get("amount")
+        transaction_amount = metadata
+            .get("amount")
             .dbg_unwrap()
             .to_owned()
             .try_into()
             .dbg_unwrap();
-        buyer_account_id = metadata.get("buyer_account_id")
+        buyer_account_id = metadata
+            .get("buyer_account_id")
             .dbg_unwrap()
             .to_owned()
             .try_into()
             .dbg_unwrap();
-    } else { return }
+    } else {
+        return;
+    }
 
     // Seek for bonds asset_definition currency id
-    if let Ok(value) = FindAssetDefinitionById(iroha_trigger::data_model::query::asset::FindAssetDefinitionById::new(bond_asset_definition_id.clone())).execute() {
+    if let Ok(value) = FindAssetDefinitionById(
+        iroha_trigger::data_model::query::asset::FindAssetDefinitionById::new(
+            bond_asset_definition_id.clone(),
+        ),
+    )
+    .execute()
+    {
         if let Ok(bonds_asset_definition) = TryInto::<AssetDefinition>::try_into(value) {
-            match bonds_asset_definition.metadata().get("currency_asset_definition_id") {
+            match bonds_asset_definition
+                .metadata()
+                .get("currency_asset_definition_id")
+            {
                 Some(value) => {
-                    if let Ok(currency_asset_definition_id) = TryInto::<AssetDefinitionId>::try_into(value.to_owned()) {
+                    if let Ok(currency_asset_definition_id) =
+                        TryInto::<AssetDefinitionId>::try_into(value.to_owned())
+                    {
                         bonds_currency_asset_definition_id = currency_asset_definition_id
-                    } else { return }
+                    } else {
+                        return;
+                    }
                 }
-                _ => return
+                _ => return,
             }
-        } else { return }
-    } else { return }
-
-    let buyer_money_asset_id = AssetId::new(bonds_currency_asset_definition_id, buyer_account_id.clone());
-
-    // Checks:
-    // * The buyer's account has the bonds currency asset.
-    // * The AssetValue has a NumericValue type
-    // * The buyer's account has enough quantity of bond asset for transaction.
-    if let Ok(value) = QueryBox::FindAssetById(FindAssetById::new(buyer_money_asset_id.clone())).execute() {
-        match TryInto::<Asset>::try_into(value) {
-            Ok(currency_asset) => {
-                match TryInto::<NumericValue>::try_into(currency_asset.value().to_owned()) {
-                    Ok(currency_asset_amount) => if currency_asset_amount < transaction_amount {return},
-                    _=> return
-                }
-            }
-            _=> return
+        } else {
+            return;
         }
+    } else {
+        return;
     }
+
+    let buyer_money_asset_id =
+        AssetId::new(bonds_currency_asset_definition_id, buyer_account_id.clone());
 
     let seller_bonds_asset_id = AssetId::new(bond_asset_definition_id, owner.clone());
 
-
-    // Checks:
-    // * The Trigger owner account has this asset.
-    // * The AssetValue has a NumericValue type
-    // * The Trigger owner account has enough asset quantity for transaction.
-    if let Ok(value) = QueryBox::FindAssetById(FindAssetById::new(seller_bonds_asset_id.clone())).execute() {
-        match TryInto::<Asset>::try_into(value) {
-            Ok(bond_asset) => {
-                match TryInto::<NumericValue>::try_into(bond_asset.value().to_owned()) {
-                    Ok(bond_asset_value) => if bond_asset_value < bonds_quantity {return},
-                    _=> return
-                }
-            }
-            _=> return
-        }
-    }
+    if !are_assets_enough(buyer_money_asset_id.clone(), &transaction_amount, seller_bonds_asset_id.clone(), &bonds_quantity) {return;}
 
     // Send money from buyer to seller
     // and then send bonds from seller to buyer
-    InstructionExpr::Transfer(TransferExpr::new(buyer_money_asset_id, transaction_amount, owner.clone())).execute().expect("Send money validation has been failed");
-    InstructionExpr::Transfer(TransferExpr::new(seller_bonds_asset_id, bonds_quantity, buyer_account_id)).execute().expect("Send bonds validation has been failed");
+    InstructionExpr::Transfer(TransferExpr::new(
+        buyer_money_asset_id,
+        transaction_amount,
+        owner.clone(),
+    ))
+    .execute()
+    .expect("Send money validation has been failed");
+    InstructionExpr::Transfer(TransferExpr::new(
+        seller_bonds_asset_id,
+        bonds_quantity,
+        buyer_account_id,
+    ))
+    .execute()
+    .expect("Send bonds validation has been failed");
+}
 
+// Checks:
+// * The Account has this asset.
+// * The AssetValue has a NumericValue type
+// * The Account has enough asset quantity for transaction.
+fn check_account_asset_amount(
+    asset_id: AssetId,
+    asset_amount: &NumericValue,
+) -> Result<bool, Box<dyn Error>> {
+    if let Ok(value) = QueryBox::FindAssetById(FindAssetById::new(asset_id)).execute() {
+        match TryInto::<Asset>::try_into(value) {
+            Ok(owned_asset) => {
+                match TryInto::<NumericValue>::try_into(owned_asset.value().to_owned()) {
+                    Ok(owned_asset_value) => {
+                        if owned_asset_value < *asset_amount {
+                            Err("The asset owner has not enough asset value".into())
+                        } else {
+                            Ok(true)
+                        }
+                    }
+                    _ => Err("The asset value has not a NumericValue type".into()),
+                }
+            }
+            // Should be impossible to reach
+            _ => Err("The found asset has not an Asset type".into()),
+        }
+    } else {
+        Err("The asset cannot be found by ID".into())
+    }
+}
 
-
+fn are_assets_enough(
+    buyer_money_asset_id: AssetId,
+    transaction_amount: &NumericValue,
+    seller_bonds_asset_id: AssetId,
+    bonds_quantity: &NumericValue,
+) -> bool {
+    check_account_asset_amount(buyer_money_asset_id, transaction_amount).dbg_unwrap()
+        == check_account_asset_amount(seller_bonds_asset_id, bonds_quantity).dbg_unwrap()
 }
